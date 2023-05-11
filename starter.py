833fe4b4c9ac38a598d3037aa11f2368d8d28f70
Dmitriy Krainov <92866398+Crynow300@users.noreply.github.com>
2023-05-11 14:09:57 +0300

    Update starter.py

--- starter.py
+++ starter.py
@@ -3,4 +3,4 @@ import os
 with open('NVR.txt') as f:
     for line in f:
         ip_addr, hostname = line.strip().split()
-        os.system(f'python3 hikvision-isapi-test.py {ip_addr} {hostname}')
+        os.system(f'python3 hikvision-isapi-zabbix-sending.py {ip_addr} {hostname}')
